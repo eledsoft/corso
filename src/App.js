a48ad924978e87d18d3726de9b888727180da08a
Enrico Leder <e.leder@ardesia.it>
2020-06-04 17:22:49 +0200

    cap 19

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 
 
@@ -9,21 +11,49 @@ class App extends React.Component {
     state = {
         users: [],
         loading: false,
+        alert: null
     }
-    async componentDidMount() {
+    /*async componentDidMount() {
+        console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
         this.setState({loading: true});
-        const res = await axios.get('https://api.github.com/users');
+        const res = await axios.get('https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}');
         console.log(res.data);
         this.setState({users: res.data, loading: false} );
+    }*/
+
+    searchUsers = async text => {
+        //console.log(text);
+        this.setState({loading: true});
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        this.setState({users: res.data.items, loading: false} );
+        console.log(res.data.items);
     }
 
+    //clear users from state
+    clearUsers = () => this.setState({users: [], loading: false});
+
+    //Set alert
+    setAlert = (msg, type) => {
+        this.setState({alert: {msg: msg, type: type} });
+    }
+
+
     render() {
+        const {users, loading} = this.state;
 
         return (
             < div className='App'>
                 <NavBar title='Github Finder' icon='fab fa-github'/>
                 <div className='container'>
-                    <Users loading={this.state.loading} users={this.state.users}/>
+                    <Alert alert={this.state.alert} />
+                    <Search
+                        searchUsers={this.searchUsers}
+                        clearUsers={this.clearUsers}
+                        showClear={users.length>0 ? true: false}
+                        setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users}/>
                 </div>
 
             </div>
